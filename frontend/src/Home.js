d11613f26057a31c61270136173406bef731c1e7
Noushin Iqra <noushinahmed95@gmail.com>
2018-10-27 05:19:29 -0400

    completed basic survey

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -9,7 +9,10 @@ class Home extends Component {
           <h2>Survey</h2>
         <form>
          <Question questionText="Where are you coming from?"/>
-
+         <ul>
+          <li><input onChange={this.handleOriginChange} type="radio" name="origin" value="ny" />New York</li>
+          <li><input onChange={this.handleOriginChange} type="radio" name="origin" value="notNy" />Not New York</li>
+        </ul>
 
          <Question questionText="What did you do in the park?"/>
          <ul>
@@ -20,7 +23,19 @@ class Home extends Component {
           </ul>
          
          <Question questionText="Was it your first time in the park?"/>
+         <ul>
+          <li><input onChange={this.handleFreqChange} type="radio" name="freq" value="yes" />Yes</li>
+          <li><input onChange={this.handleFreqChange} type="radio" name="freq" value="no" />No</li>
+        </ul>
+
          <Question questionText="What part of the park did you visit?"/>
+         <ul>
+          <li><input onChange={this.handleLocationChange} type="radio" name="location" value="north" />North</li>
+          <li><input onChange={this.handleLocationChange} type="radio" name="location" value="south" />South</li>
+          <li><input onChange={this.handleLocationChange} type="radio" name="location" value="west" />West</li>
+          <li><input onChange={this.handleLocationChange} type="radio" name="location" value="east" />East</li>
+        </ul>
+
          </form>
         </div>
 
