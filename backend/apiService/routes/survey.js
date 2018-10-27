5cf708d0707224375c74594924c37298db03349f
Timothy Chow <timchow26@yahoo.com>
2018-10-27 03:40:16 -0400

    cleaned up formatting

--- backend/apiService/routes/survey.js
+++ backend/apiService/routes/survey.js
@@ -1,15 +1,19 @@
 var express = require('express');
 var router = express.Router();
 var MongoClient = require('mongodb').MongoClient;
+
 const mongoose = require('mongoose');
 const Survey = mongoose.model('survey');
 mongoose.connect("mongodb://35.170.187.47:27017/survey");
+
 router.route('/').post(function(req,res,next) {
+
     var gender = req.body.gender;
     var outOfState = req.body.outOfState;
     var activity = req.body.activity;
     var firstTime = req.body.firstTime;
     var part = req.body.part;
+
     const survey = new Survey({
         gender,
         outOfState,
@@ -17,8 +21,11 @@ router.route('/').post(function(req,res,next) {
         firstTime,
         part
     });
+
     console.log(survey.gender);
     survey.save().then(function () {});
     res.send();
+
 });
+
 module.exports = router;
