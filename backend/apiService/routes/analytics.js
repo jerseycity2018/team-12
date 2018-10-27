0ad101814d960a5e0e65d7b16eb40b74ce0f3fb8
Timothy Chow <timchow26@yahoo.com>
2018-10-27 08:44:16 -0400

    added tourist vs. resident graph

--- backend/apiService/routes/analytics.js
+++ backend/apiService/routes/analytics.js
@@ -102,6 +102,80 @@ router.get('/activity', function (req, res, next) {
     });
 });
 
+
+
+router.get('/dateNYC', function (req, res, next) {
+    Survey.find({}).then(function (users) {
+
+
+        let January = 0;
+        let JanuaryFirst = 0;
+
+        let February = 0;
+        let FebruaryFirst = 0;
+
+        let March = 0;
+        let MarchFirst = 0;
+
+        let April = 0;
+        let AprilFirst = 0;
+
+        let May = 0;
+        let MayFirst = 0;
+
+        let June = 0;
+        let JuneFirst = 0;
+
+        let August = 0;
+        let AugustFirst = 0;
+
+        let September = 0;
+        let SeptemberFirst = 0;
+
+        let November = 0;
+        let NovemberFirst = 0;
+
+        let December = 0;
+        let DecemberFirst = 0;
+
+
+        const arrNYC= [];
+        const arrNot = [];
+
+
+        users.forEach((user) => {
+
+            var tempBoolean = user.outOfState;
+            var tempNum = user.date.getMonth();
+            console.log(tempNum);
+            console.log(user.date);
+
+            if (tempBoolean){
+
+
+                if (arrNYC[tempNum] === undefined) {
+                    arrNYC[tempNum] = 0;
+                }
+                arrNYC[tempNum]++;
+
+            } else {
+
+                if (arrNot[tempNum] === undefined) {
+                    arrNot[tempNum] = 0;
+                }
+                arrNot[tempNum]++;
+
+            }
+
+
+    });
+        res.send({
+            arrNYC,
+            arrNot
+        });
+});
+});
+
 router.get('/heat', function (req, res, next) {
     Survey.find({}).then(function (users) {
         const heatArray = [];
