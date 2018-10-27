0d60fa1d5de723ebdda4af6302f83b3311d30222
Sunny Ruan <jiaqingr@gmail.com>
2018-10-27 07:16:46 -0400

    Add styling to registration page

--- frontend/src/UserRegistration.js
+++ frontend/src/UserRegistration.js
@@ -1,37 +1,74 @@
 import React, { Component } from "react";
-
+import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import "./UserRegistration.css";
 
 
 class UserRegistration extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            username: "",
+            email: "",
+            password: ""
+        };
+    }
+
+    validateForm() {
+        return this.state.username.length > 0 && this.state.email.length > 0 && this.state.password.length > 0;
+    }
+
+    handleChange = event => {
+        this.setState({
+            [event.target.id]: event.target.value
+        });
+    }
+
+    handleSumit = event => {
+        event.preventDefault();
+    }
+
     render() {
          return(
-            <div className="App">
-                <header className="App-header">
+            <div className="Register">
+            <form onSubmit={this.handleSubmit}>
+                <FormGroup controlId="username" bsSize="large">
+                    <ControlLabel>Username</ControlLabel>
+                    <FormControl
+                        autoFocus
+                        type="username"
+                        value={this.state.username}
+                        onChange={this.handleChange}
+                    />
+                </FormGroup>
+                <FormGroup controlId="email" bsSize="large">
+                    <ControlLabel>Email</ControlLabel>
+                    <FormControl
+                        autoFocus
+                        type="email"
+                        value={this.state.email}
+                        onChange={this.handleChange}
+                    />
+                </FormGroup>
+                <FormGroup controlId="password" bsSize="large">
+                <ControlLabel>Password</ControlLabel>
+                <FormControl
+                    value={this.state.password}
+                    onChange={this.handleChange}
+                    type="password"
+                />
+                </FormGroup>
+                <Button
+                    block
+                    bsSize="large"
+                    disabled={!this.validateForm()}
+                    type="submit"
+                >
+                    Register
+                </Button>
+            </form>
                 
-                <h1>Welcome Joining Us!</h1>
-                <form>
-                    <label>
-                        Username:
-                        <input type="text" name="frame" />
-                    </label>
-                </form>
-                <form>
-                    <label>
-                        Email:
-                        <input type="text" name="frame" />
-                    </label>
-                </form>
-                <form>
-                    <label>
-                        Password:
-                        <input type="password"  id="myPassword" />
-                    </label>
-                </form>
-                <input type="submit" value="Submit" />
-                
-
-                </header>
-            </div>
+            </div> 
          );
     }
 }
