e20347eb97ac2007dae263205e25cd0974a24bd8
Noushin Iqra <noushinahmed95@gmail.com>
2018-10-26 23:52:50 -0400

    test homepage

--- frontend/cpcsolution/src/App.js
+++ frontend/cpcsolution/src/App.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './images.jpg';
 import './App.css';
 import Custom from './Custom.js';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      
+
+      <div class="container" className="App">
+
+
         <header className="App-header">
+
+        <div row="col-xs-12 col-md-12" id="navbar">
+        <Navbar></Navbar>
+        <button type="button" class="btn">Register</button>
+        </div>
+
+        <div row="col-xs-12 col-md-12">
           <img src={logo} className="App-logo" alt="logo" />
+        </div>
+        <div>
           <p>
             Edit <code>src/App.js</code> and save to relsdaklfj ak;lsdfj
           </p>
@@ -21,9 +34,10 @@ class App extends Component {
             Learn React
           </a>
           <Custom myName="John"/>
+          </div>
         </header>
-      </div>
-    );
+      </div>//app
+    ); // return
   }
 }
 
