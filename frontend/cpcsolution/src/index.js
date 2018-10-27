cc2b2cbc07fa11fa99838ed77d3edd95a1521375
Liza Liutova <liza@Lizas-MacBook-Air.local>
2018-10-27 01:21:14 -0400

    made changes to menu style

--- frontend/cpcsolution/src/index.js
+++ frontend/cpcsolution/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './menu.css'
 import App from './App';
 import Main from './Main'
 import * as serviceWorker from './serviceWorker';
