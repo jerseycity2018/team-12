ac98663770bf68eeee88d5ef8c347f93c28fcdfa
Johnny Lao <csgodicer@gmail.com>
2018-10-26 23:07:08 -0400

    start on analytics page

---
+++ frontend/cpcsolution/src/routes/Analytics.jsx
@@ -0,0 +1,11 @@
+import React, {PureComponent} from 'react';
+import "~react-vis/dist/style";
+import {XYPlot, XAxis, YAxis, HorizontalGridLines, LineSeries} from 'react-vis';
+
+class Analytics extends PureComponent {
+
+
+    render() {
+
+    }
+}
