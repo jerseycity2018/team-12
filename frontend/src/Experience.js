e7992b314404f5800de12a7493d1594202ba7dcf
Liza Liutova <liza@Lizas-MacBook-Air.local>
2018-10-27 06:56:04 -0400

    added date

--- frontend/src/Experience.js
+++ frontend/src/Experience.js
@@ -3,9 +3,14 @@ import './experienceBoxes.css';
 class Experience extends React.Component {
     render() {
       return <li>
+      <div class="right">
+        <p>{this.props.Time}</p>
+      </div>
       <img src={this.props.imageSrc}/>
-      <h1> {this.props.Title} </h1>
-      <p>{this.props.Descr}</p>
+      <div class="left"><h1> {this.props.Title} </h1>
+
+      <p>{this.props.Descr}</p></div>
+
       </li>;
     }
 }
--- frontend/src/Experiences.js
+++ frontend/src/Experiences.js
@@ -9,9 +9,15 @@ class Experiences extends Component {
       <div>
         <h2>EXPERIENCES</h2>
         <ul>
-          <Experience imageSrc = {img} Title = "Yoga" Descr="Take outdoor yoga classes in Central Park. All levels welcome. Enjoy the perfect setting to do yoga on the grass in New York City and be in harmony with nature."/>
-          <Experience imageSrc = {pug} Title = "Pug Meetup" Descr="A meeting of Pug loving minds! Both you and your posts will be immediately removed if you join our group with intentions of soliciting members for breeding purposes or selling pug puppies."/>
-          <Experience imageSrc = {taichi} Title = "Tai Chi" Descr="Welcome to the World Tai chi boxing association, NYC branch. This class will teach self healing and self defense in the Old yang style tai chi system as developed by Erle Montaigue."/>
+          <Experience imageSrc = {img} Title = "Yoga"
+          Descr="Take outdoor yoga classes in Central Park. All levels welcome. Enjoy the perfect setting to do yoga on the grass in New York City and be in harmony with nature."
+          Time="2018-11-02"/>
+          <Experience imageSrc = {pug} Title = "Pug Meetup"
+          Descr="A meeting of Pug loving minds! Both you and your posts will be immediately removed if you join our group with intentions of soliciting members for breeding purposes or selling pug puppies."
+            Time="2018-11-10"/>
+          <Experience imageSrc = {taichi} Title = "Tai Chi"
+          Descr="Welcome to the World Tai chi boxing association, NYC branch. This class will teach self healing and self defense in the Old yang style tai chi system as developed by Erle Montaigue."
+            Time="2018-11-14"/>
 
         </ul>
       </div>
