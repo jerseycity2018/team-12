0f1b359a09b756bbcbf0e61bc905e0c92fd3b249
Timothy Chow <timchow26@yahoo.com>
2018-10-27 06:00:32 -0400

    added class for heatmap

---
+++ frontend/src/Map.js
@@ -0,0 +1,14 @@
+/**
+ * Created by timothy on 10/27/18.
+ */
+
+import React from 'react';
+import { render } from 'react-dom';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import HeatmapLayer from '../src/HeatmapLayer';
+
+class Map extends React.Component {
+
+
+
+}
